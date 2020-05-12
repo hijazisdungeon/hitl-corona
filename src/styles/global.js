aca498b8dd53a04e7bf7744bb28c0e7869a007b2
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-05-12 18:16:08 -0300

    fixes

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   body {
-    font: 1.6rem 'Montserrat', sans-serif;
     color: ${props => props.theme.color};
     background-color: ${props => props.theme.background};
   }
