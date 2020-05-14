9d79d5602ec139a3e2e486ed2e3d24fe1aba906c
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-05-14 18:18:13 -0300

    About us remasterized

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,28 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaDiscord } from 'react-icons/fa';
+import { FiChevronDown } from 'react-icons/fi';
 
 import Layout from '~/layouts/Default';
 
 import Head from '~/components/Head';
+import Link from '~/components/Link';
+import Slider from '~/components/Slider';
 
 import { githubApi } from '~/services/api';
 
 import {
   Container,
-  Header,
+  Content,
+  Section,
   MembersContainer,
   Row,
   Brands,
+  BrandIcons,
 } from '~/styles/pages/aboutus';
 
 const discordMembers = {
   devhijazi: {
-    username: 'Hijazi',
-    discriminator: '#0808',
+    username: 'Gabriel Hijazi',
+    discord_link: 'https://discord.com/users/221865218840592384',
   },
   pablo1v: {
-    username: 'pablo1v',
-    discriminator: '#1177',
+    username: 'Pablo Vinícius',
+    discord_link: 'https://discord.com/users/606909018861994026',
   },
 };
 
@@ -56,34 +61,67 @@ const AboutUsPage = () => {
 
       <Layout loading={!members.length}>
         <Container>
-          <Header>
-            <p>
-              Site pensado e desenvolvido pela equipe <b>SweetCode</b>.
-            </p>
-          </Header>
+          <Content>
+            <Slider
+              images={[
+                '/static/images/aboutus/slider_01.jpg',
+                '/static/images/aboutus/slider_02.jpg',
+                '/static/images/aboutus/slider_03.jpg',
+              ]}
+            />
+            <div>
+              <h1>Sweetcode</h1>
+              <p>
+                Desenvolvedora oficial do <span>Covid Agora</span>
+              </p>
+              <span>
+                <Link href="#team">
+                  <FiChevronDown size={55} color="#fff" />
+                </Link>
+              </span>
+            </div>
+          </Content>
+          <Section id="team">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path
+                fill="#23272a"
+                fillOpacity="1"
+                d="M0,160L12.6,133.3C25.3,107,51,53,76,64C101.1,75,126,149,152,154.7C176.8,160,202,96,227,85.3C252.6,75,278,117,303,128C328.4,139,354,117,379,117.3C404.2,117,429,139,455,133.3C480,128,505,96,531,96C555.8,96,581,128,606,122.7C631.6,117,657,75,682,90.7C707.4,107,733,181,758,208C783.2,235,808,213,834,202.7C858.9,192,884,192,909,170.7C934.7,149,960,107,985,106.7C1010.5,107,1036,149,1061,138.7C1086.3,128,1112,64,1137,42.7C1162.1,21,1187,43,1213,69.3C1237.9,96,1263,128,1288,128C1313.7,128,1339,96,1364,96C1389.5,96,1415,128,1427,144L1440,160L1440,320L1427.4,320C1414.7,320,1389,320,1364,320C1338.9,320,1314,320,1288,320C1263.2,320,1238,320,1213,320C1187.4,320,1162,320,1137,320C1111.6,320,1086,320,1061,320C1035.8,320,1011,320,985,320C960,320,935,320,909,320C884.2,320,859,320,834,320C808.4,320,783,320,758,320C732.6,320,707,320,682,320C656.8,320,632,320,606,320C581.1,320,556,320,531,320C505.3,320,480,320,455,320C429.5,320,404,320,379,320C353.7,320,328,320,303,320C277.9,320,253,320,227,320C202.1,320,177,320,152,320C126.3,320,101,320,76,320C50.5,320,25,320,13,320L0,320Z"
+              />
+            </svg>
+            <h1>DESENVOLVEDORES</h1>
+            <MembersContainer>
+              {members.map(member => (
+                <Row key={member.id}>
+                  <img src={member.avatar_url} alt="Member Logo" />
 
-          <MembersContainer>
-            {members.map(member => (
-              <Row key={member.id}>
-                <img src={member.avatar_url} alt="Member Logo" />
-
-                <Brands>
-                  <span>
-                    {member.username}
-                    <span>{member.discriminator}</span>
-                  </span>
-
-                  <a
-                    href={member.html_url}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    <FaGithub size="2.8rem" color="#fff" />
-                  </a>
-                </Brands>
-              </Row>
-            ))}
-          </MembersContainer>
+                  <Brands>
+                    <span>{member.username}</span>
+                  </Brands>
+                  <BrandIcons>
+                    <span>
+                      <a
+                        href={member.html_url}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaGithub size={35} color="#000" />
+                      </a>
+                    </span>
+                    <span>
+                      <a
+                        href={member.discord_link}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaDiscord size={35} color="#7289DA" />
+                      </a>
+                    </span>
+                  </BrandIcons>
+                </Row>
+              ))}
+            </MembersContainer>
+          </Section>
         </Container>
       </Layout>
     </>
--- src/styles/pages/aboutus.js
+++ src/styles/pages/aboutus.js
@@ -1,31 +1,88 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  display: flex;
+  position: relative;
   width: 100%;
-  height: 100%;
-  min-height: 100vh;
-  padding: 20px;
   text-align: center;
-  flex-direction: column;
-  align-items: center;
   justify-content: center;
-  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-    url('/static/images/aboutus/background.jpg') no-repeat;
-  background-position: 50% 50%;
-  background-size: cover;
-  background-attachment: fixed;
 `;
 
-export const Header = styled.header`
-  margin-top: 120px;
-  img {
-    width: 20rem;
-    height: auto;
+export const Content = styled.section`
+  position: relative;
+  height: 100vh;
+  height: 100vh;
+
+  & > section {
+    position: absolute;
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+    left: 0;
   }
-  p {
-    margin-top: 20px;
+
+  & > div {
+    position: relative;
+    display: flex;
+    width: 100%;
+    height: 100%;
+
+    z-index: 2;
+    padding: 20px 40px;
+
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8));
+    h1 {
+      font-size: 8rem;
+      text-align: center;
+      @media (max-width: 515px) {
+        display: flex;
+        flex-direction: column;
+      }
+    }
+    p {
+      font-size: 3rem;
+      span {
+        color: #57bcff;
+      }
+    }
+    & > span {
+      margin-top: 30px;
+      a {
+        display: flex;
+        padding: 10px;
+
+        border-radius: 20%;
+        border: 2px solid #006daa;
+        &:hover {
+          transition-duration: 400ms;
+          border-color: #fff;
+        }
+      }
+    }
+  }
+`;
+export const Section = styled.section`
+  position: relative;
+  display: flex;
+  padding: 120px 0;
+
+  flex-direction: column;
+  align-items: Center;
+  justify-content: center;
+  background-color: #f5f5f5;
+
+  h1 {
     font-size: 4rem;
+    color: #000;
+    margin-bottom: 5px;
+  }
+  & > svg {
+    position: absolute;
+    z-index: 0;
+    bottom: 0;
+    fill: #23272a;
   }
 `;
 
@@ -33,17 +90,17 @@ export const MembersContainer = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
-  margin-top: 80px;
+  margin-top: 5px;
 `;
 
 export const Row = styled.div`
   width: 25rem;
-  margin: 30px 50px;
+  margin: 60px 50px;
   transition: transform 300ms;
   img {
     width: 20rem;
     height: 20rem;
-    border-radius: 50%;
+    border-radius: 20%;
     box-shadow: 0 2px 8px 2px rgba(0, 0, 0, 0.3);
   }
   &:hover {
@@ -60,12 +117,14 @@ export const Brands = styled.div`
   padding: 10px 20px;
   align-items: center;
   justify-content: space-between;
+
   border-radius: 5px;
   box-shadow: 0 2px 8px 2px rgba(0, 0, 0, 0.3);
-  background: #0353a4;
+  background: #006daa;
   span {
     font-size: 2rem;
     font-weight: 500;
+
     span {
       color: #000;
       font-weight: 600;
@@ -75,5 +134,16 @@ export const Brands = styled.div`
     svg {
       color: #b9d6f2 !important;
     }
+  }
+`;
+export const BrandIcons = styled.div`
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    align-items: center;
+    margin: 5px;
   }
 `;
